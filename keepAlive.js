76f14c073f300b28d94e706b3f41014f8e87c43c
ItzSidhan <89626058+ItzSidhan@users.noreply.github.com>
2021-10-31 10:09:41 +0530

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const server = express();
- 
-server.all('/', (req, res) => {
-  res.send(`OK`)
+const express = require("express")
+
+const server = express()
+
+server.all("/", (req, res) => {
+  res.send('<meta http-equiv="refresh" content="0; URL=https://itzsidhan.gitbook.io/itzsidhan/support"/>')
 })
- 
+
 function keepAlive() {
-  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+  server.listen(3000, () => {
+    console.log("Server is ready.")
+  })
 }
- 
-module.exports = keepAlive;
+
+module.exports = keepAlive
